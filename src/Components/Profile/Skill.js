4b0b66023bbc0a288e3160765cc2113cdc4c8c83
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-04-05 17:52:16 +0530

    profile

--- src/Components/Profile/Skill.js
+++ src/Components/Profile/Skill.js
@@ -8,7 +8,7 @@ export default function Skill(props) {
       }
     
   return (
-    <div className="col-md-7">
+    <div>
     <br />
     <div>
       <h1>General Skills</h1>
@@ -19,18 +19,6 @@ export default function Skill(props) {
         <div className="row">
           <div className="col-md-12">
             <Card.Body>
-              {/* <div>
-                <h4>Oral Communication</h4>
-                <Stack spacing={3} direction="row">
-
-                  <Buttons variant="outlined"  >0 <br /> <>Not required</></Buttons>
-                  <Buttons variant="outlined">1 <br /> <>No Capability</></Buttons>
-                  <Buttons variant="outlined">2 <br /> <>Basic Level</></Buttons>
-                  <Buttons classname='Rating' variant="outlined">3 <br /> <>Intermediate Level</></Buttons>
-                  <Buttons variant="outlined">4 <br /> <>Advanced Level</></Buttons>
-                  <Buttons variant="outlined">5 <br /> <>Expert/SME</></Buttons>
-                </Stack>
-              </div> <br /> <hr /> */}
               <div className="wrapper">
                 
               <div className="skill">
